5fc9b3efa0aeadbece85a4141b3e4adb7878735d
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-12 14:19:38 -0500

    Adding githooks

---
+++ .githooks/commit-msg/2disallowMessage.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const util = require('./../util.js'),
+    _ = require('lodash'),
+
+    // The amount of words needed including the prefix
+    minimumWordCount = 5,
+
+    // Word count for the preappended prefix to every commit message
+    prefixWords = 2;
+
+return util.getCommitMessage()
+    .then((commitMessage) => {
+        const wordCount = commitMessage.split(' ').length;
+        let errorString = [];
+
+        if (wordCount < minimumWordCount) {
+            errorString.push(`Commit Message: "${commitMessage}" Not Descriptive. Please use at least ${minimumWordCount - prefixWords} words after prefix`);
+        }
+
+        if (errorString.length > 0) {
+            // http://misc.flogisoft.com/bash/tip_colors_and_formatting
+            console.log('\n');
+            console.log('\t\x1b[103m', _.join(errorString, '\n') ,'\x1b[0m');
+            console.log('\t\x1b[31m', 'Aborting Commit.' ,'\x1b[0m');
+            console.log('\n');
+            process.exit(1);
+        }
+    });
---
+++ .githooks/util.js
@@ -0,0 +1,71 @@
+const exec = require('child_process').exec,
+    fs = require('fs'),
+    constants = require('./const.js'),
+    _ = require('lodash');
+
+module.exports = {
+    bash: function(command) {
+        return new Promise((resolve) => {
+            exec(command, (error, stdout) => {
+                resolve({
+                    error: error,
+                    output: _.trim(stdout, '\n')
+                })
+            });
+
+        });
+    },
+
+    getBranchName: function() {
+        return this.bash('git symbolic-ref -q HEAD')
+          .then((result) => {
+              branchName = _(result.output)
+                .split('/')
+                .last()
+                .trim(['\n'])
+
+              return branchName;
+          })
+    },
+
+    isBranchNamePrefixed: function(branchName) {
+        return !_.isNil(this.getPrefix(branchName));
+    },
+
+    isCommitMsgPrefixed: function(commitMessage) {
+        return !_.isNil(this.getPrefix(commitMessage));
+    },
+
+    getPrefix: function(string) {
+        string = _.lowerCase(string);
+        const prefix = _(constants.BRANCH_PREFIXES)
+            .find((prefix) => {
+                return _.startsWith(string, prefix)
+            })
+
+        return prefix;
+    },
+
+    writeCommitMessage: function(message) {
+      return new Promise((resolve) => {
+          fs.writeFile(constants.COMMIT_MSG_PATH, message, function (err,data) {
+              if (err) {
+                  return console.log(err);
+              }
+              resolve();
+          });
+      })
+    },
+
+    getCommitMessage: function() {
+      return new Promise((resolve) => {
+          fs.readFile(constants.COMMIT_MSG_PATH, 'utf8', function(err, contents) {
+             const commitMessage = _(contents)
+                .split('\n')
+                .head()
+
+              resolve(commitMessage);
+          });
+      })
+    }
+}
